87385dff9e7e9e05fa80f5d00fceafc9b5ebfa7a
felipelagares <62161177+felipelagares@users.noreply.github.com>
2024-01-11 19:48:13 -0300

    Update Agendas.java

--- src/main/java/negocio/Agendas.java
+++ src/main/java/negocio/Agendas.java
@@ -2,12 +2,32 @@ package negocio;
 import java.time.LocalDate;
 public class Agendas {
     private LocalDate data;
-    private String situacao;
+    private EnumEstadoAgendamento situacao;
     private LocalDate data_situacao;
 
-    public Agendas(LocalDate data, LocalDate dataSituacao) {
+    private  User paciente;
+    private Vacina vacina;
+
+    public enum EnumEstadoAgendamento{
+        AGENDADO(1), REALIZADO(0), CANCELADO(-1);
+        private int valor;
+        EnumEstadoAgendamento(int valorOpcao) {
+            valor = valorOpcao;
+        }
+
+        public int getValor(){
+            return valor;
+        }
+    }
+    public Agendas(LocalDate data) {
         this.data = data;
-        this.situacao = "Agendado";
-        this.data_situacao = dataSituacao;
+        this.situacao = EnumEstadoAgendamento.AGENDADO;
+        this.data_situacao = LocalDate.now();
+    }
+    public void cancelar(){
+        this.situacao = EnumEstadoAgendamento.CANCELADO;
+    }
+    public  void concluir(){
+        this.situacao = EnumEstadoAgendamento.REALIZADO;
     }
 }
