a427180c2cb12ca5e6a9dbacb8d7a451fc7926a9
felipelagares <felipegalvaolagares2@gmail.com>
2024-01-15 19:47:04 -0300

    Update Alergia.java

--- src/main/java/negocio/Alergia.java
+++ src/main/java/negocio/Alergia.java
@@ -1,7 +1,13 @@
 package negocio;
 
 public class Alergia {
+
+    private int id;
     private String name;
+
+    public int getId() {
+        return id;
+    }
 
     public String getName() {
         return name;
