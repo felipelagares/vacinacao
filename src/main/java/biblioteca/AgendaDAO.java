e8d558ef01af8f984e13bae488c3814978b25746
felipelagares <62161177+felipelagares@users.noreply.github.com>
2024-01-18 15:44:41 -0300

    Update AgendaDAO.java

--- src/main/java/biblioteca/AgendaDAO.java
+++ src/main/java/biblioteca/AgendaDAO.java
@@ -3,6 +3,7 @@ package biblioteca;
 import negocio.Agendas;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
@@ -18,9 +19,11 @@ public class AgendaDAO {
             String sql = "INSERT INTO User(id, data, situacao, data_situacao, paciente_id, vacina_id) VALUES (?, ?, ?, ?, ?, ?)";
             PreparedStatement instrucao = this.conexao.prepareStatement(sql);
             instrucao.setInt(1, a.getId());
-            instrucao.setDate(2, a.getData());
+            Date dataSql = Date.valueOf(a.getData());
+            instrucao.setDate(2, dataSql);
             instrucao.setInt(3, a.getSituacao());
-            instrucao.setDate(4, a.getData_situacao());
+            Date dataSituationSql = Date.valueOf(a.getData_situacao());
+            instrucao.setDate(4, dataSituationSql);
             instrucao.setInt(5, a.getPacienteId());
             instrucao.setInt(6, a.getVacinaId());
             instrucao.execute();
