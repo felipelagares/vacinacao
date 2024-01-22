7317a62030033a8bf1a1ad356bb81c0abcc3e26a
felipelagares <62161177+felipelagares@users.noreply.github.com>
2024-01-21 23:38:18 -0300

    Update Agendas.java

--- src/main/negocio/Agendas.java
+++ src/main/negocio/Agendas.java
@@ -1,22 +1,31 @@
 package negocio;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDate;
+
 @Entity
 public class Agendas {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private LocalDate data;
     private EnumEstadoAgendamento situacao;
     private LocalDate data_situacao;
 
-    private  int pacienteId;
+    private int pacienteId;
+    private int vacinaId;
 
     public LocalDate getData() {
         return data;
     }
 
-    public int getSituacao() {
-        return situacao.getValor();
+    public EnumEstadoAgendamento getSituacao() {
+        return situacao;
     }
 
     public LocalDate getData_situacao() {
@@ -31,23 +40,10 @@ public class Agendas {
         return vacinaId;
     }
 
-    private int vacinaId;
-
     public int getId() {
         return id;
     }
 
-    public enum EnumEstadoAgendamento{
-        AGENDADO(1), REALIZADO(0), CANCELADO(-1);
-        private int valor;
-        EnumEstadoAgendamento(int valorOpcao) {
-            valor = valorOpcao;
-        }
-
-        public int getValor(){
-            return valor;
-        }
-    }
     public Agendas(LocalDate data, int p, int v) {
         this.pacienteId = p;
         this.vacinaId = v;
@@ -55,10 +51,25 @@ public class Agendas {
         this.situacao = EnumEstadoAgendamento.AGENDADO;
         this.data_situacao = LocalDate.now();
     }
-    public void cancelar(){
+
+    public void cancelar() {
         this.situacao = EnumEstadoAgendamento.CANCELADO;
     }
-    public  void concluir(){
+
+    public void concluir() {
         this.situacao = EnumEstadoAgendamento.REALIZADO;
+    }
+
+    public enum EnumEstadoAgendamento {
+        AGENDADO(1), REALIZADO(0), CANCELADO(-1);
+        private int valor;
+
+        EnumEstadoAgendamento(int valorOpcao) {
+            valor = valorOpcao;
+        }
+
+        public int getValor() {
+            return valor;
+        }
     }
 }
